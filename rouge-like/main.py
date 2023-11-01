a25434a38c76b8ab894f361246c281cdcb18bf89
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-10-31 21:12:34 -0500

    create player blit

--- rouge-like/main.py
+++ rouge-like/main.py
@@ -1,6 +1,8 @@
 # standard imports
 import pygame
 
+player = pygame.image.load('rouge-like/graphics/fantasy-tileset.png')
+
 def main():
     pygame.init()
     pygame.display.set_caption('rouge-like')
@@ -13,6 +15,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        scree.blit(player, (50,50))
 
 if __name__ == '__main__':
     main()
