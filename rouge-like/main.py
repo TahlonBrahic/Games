d0ed522acae9f4a9db8771111e3c4cb50c59d9bb
TahlonBrahic <tahlonbrahic@gmail.com>
2023-11-01 20:24:55 -0500

    Add player class

--- rouge-like/main.py
+++ rouge-like/main.py
@@ -1,20 +1,28 @@
 # standard imports
 import pygame
 
-def choose_player_character(player_class='knight'):
-    if player_class == 'knight':
-        tileset = pygame.image.load('graphics/fantasy-tileset.png')
-        player_model = tileset.subsurface(pygame.Rect(0, 0, 32, 32))
-        return player_model
+pygame.init()
+pygame.display.set_caption('rouge-like')
+screen = pygame.display.set_mode((640,480))
+frame_per_second = pygame.time.Clock()
+ 
+ # Player Class
+class Player(pygame.sprite.Sprite):
+    def __init__(self, player_class='knight'):
+        super().__init__()
+        self.surf = pygame.Surface((30,30))
+        self.rect = self.surf.get_rect()
+        self.player_class = player_class
 
-player = choose_player_character()        
+    def player_model(self):
+        if self.player_class == 'knight':
+            tileset = pygame.image.load('graphics/fantasy-tileset.png')
+            player_model = tileset.subsurface(pygame.Rect(64, 576, 32, 32)) # because this is a 32x32 tileset you can scroll over with 8-bit adjustments to the first two parameters, the first is row, the second is column
+            return player_model
+
+     
 
 def main():
-    pygame.init()
-    pygame.display.set_caption('rouge-like')
-
-    screen = pygame.display.set_mode((1920,1080))
-
     running = True
 
     # Game Loop
@@ -23,10 +31,13 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-    
-        screen.fill((255, 255, 255))
-        
-        screen.blit(player, (400,300))
+
+        # Drawing Entities
+        screen.blit(Player().player_model(), (320,200))
+
+        # Time and Frame 
+        pygame.display.update()
+        frame_per_second.tick(60)
 
 if __name__ == '__main__':
     main()
