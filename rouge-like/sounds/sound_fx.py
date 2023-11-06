f8295a7f8fe974b17e0495cd36b1a45778370c8c
TahlonBrahic <tahlonbrahic@gmail.com>
2023-11-05 19:04:46 -0600

    Add preliminary code

---
+++ rouge-like/sounds/sound_fx.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+import time
+
+# Background music
+# track = 
+# pygame.mixer.music.load(track)
+# pygame.mixer.music.play(-1, 0.0)
+# pygame.mixer.music.stop()
+
+# Sound Effects
