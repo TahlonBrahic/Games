24a98eaf085c21ad0f4e2a904df7cb3631b3304a
TahlonBrahic <tahlonbrahic@gmail.com>
2023-11-07 20:40:46 -0600

    Start a collision method in Block

--- rouge-like/game_entities/block.py
+++ rouge-like/game_entities/block.py
@@ -2,6 +2,12 @@ import pygame
 from pygame.locals import *
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, tile = 'wall_1'):
         super().__init__()
+        self.tile = tile
 
+    def collision(self, player):
+        if pygame.Rect.colliderect(self.rect, player.rect):
+            player.acc = 0
+            # Check which side the collision is on, if X is the collision side make self.pos.x = self.pos.x, likewise for y
+            
