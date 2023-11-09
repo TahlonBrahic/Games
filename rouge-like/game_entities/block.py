db183240b84937d1e39f4adeb680687c3d7a56ba
TahlonBrahic <tahlonbrahic@gmail.com>
2023-11-08 20:52:51 -0600

    Fully defined collision class

--- rouge-like/game_entities/block.py
+++ rouge-like/game_entities/block.py
@@ -13,6 +13,11 @@ class Block(pygame.sprite.Sprite):
         if pygame.Rect.colliderect(self.rect, player.rect):
             player.acc = 0
             # Check which side the collision is on, if X is the collision side make self.pos.x = self.pos.x, likewise for y
+            if (self.pos.x > 0) and (self.pos.x < 640) and (self.pos.y < 0) and (self.pos.y > 480): # logic that decides whether a block can be moved or not based on where it is
+                temp_x = self.pos.x 
+                temp_y = self.pos.y
+                self.move()
+                player.pos.x, player.pos.y = temp_x, temp_y
 
     def move(self):
         return
