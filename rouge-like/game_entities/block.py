392d07e95ccdf01d7049b13ce73e4635d555a6d2
TahlonBrahic <tahlonbrahic@gmail.com>
2023-11-14 19:42:03 -0600

    Remove collision in block class

--- rouge-like/game_entities/block.py
+++ rouge-like/game_entities/block.py
@@ -10,16 +10,6 @@ class Block(pygame.sprite.Sprite):
         self.pos = pos # vec(())
         self.rect = self.image.get_rect()
 
-    def collision(self, player):
-        if pygame.Rect.colliderect(self.rect, player.rect):
-            player.acc = 0
-            # Check which side the collision is on, if X is the collision side make self.pos.x = self.pos.x, likewise for y
-            if (self.pos.x > 0) and (self.pos.x < 640) and (self.pos.y < 0) and (self.pos.y > 480): # logic that decides whether a block can be moved or not based on where it is
-                temp_x = self.pos.x 
-                temp_y = self.pos.y
-                self.move()
-                player.pos.x, player.pos.y = temp_x, temp_y
-
     def move(self):
         return
     # I need logic to check collision side to be able to tell move() where to move the block
